015bcac5db17f89ab50959ac72f5b69d3d496df2
13189575001 <1121998483@qq.com>
2017-02-21 11:32:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/android_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.administrator.android_app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("hello!");
+    }
+}
